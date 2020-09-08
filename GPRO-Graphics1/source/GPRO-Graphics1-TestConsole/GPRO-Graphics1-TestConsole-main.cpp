966ab7d303210dd5b811c5986959812ee0ec213a
ryan.burns <ryan.burns@mymail.champlain.edu>
2020-09-07 23:14:57 -0400

    Finished Chapter 2

--- GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.cpp
+++ GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.cpp
@@ -28,6 +28,7 @@
 
 #include <string>
 #include <fstream>
+#include <iostream>
 
 #include "gpro/gpro-math/gproVector.h"
 
@@ -61,10 +62,33 @@ int main(int const argc, char const* const argv[])
 
 #ifdef __cplusplus
 
-	std::ofstream file("image.txt");
-	std::string test = "Hello.";
+	const int image_width = 256;
+	const int image_height = 256;
 
-	file << test << std::endl;
+	std::ofstream file("image.ppm");
+
+	file << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; j--)
+	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+
+		for (int i = 0; i < image_width; i++)
+		{
+			double r = double(i) / static_cast<double>(image_width - 1);
+			double g = double(j) / static_cast<double>(image_height - 1);
+			double b = 0.25f;
+
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			file << ir << ' ' << ig << ' ' << ib << '\n';
+		}
+	}
+
+	std::cerr << "\nDone.\n";
+
 	file.close();
 
 #else
